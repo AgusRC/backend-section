0751db998a5a63474daf0afdb426730862de8bd4
AgusRC <agustinmetal_@hotmail.com>
2021-07-21 17:40:39 -0500

    [ADD] mongoose-autopopulate dependency added

--- src/startup/container.js
+++ src/startup/container.js
@@ -14,6 +14,9 @@ const { HomeController } = require("../controllers");
 const { HomeRoutes } = require("../routes/index.routes");
 const Routes = require("../routes")
 
+// models
+const {User, Comment, Idea} = require("../models")
+
 const container = createContainer();
 
 container
@@ -31,5 +34,10 @@ container
 .register({
     HomeRoutes: asFunction(HomeRoutes).singleton()
 })
+.register({
+    User: asValue(User),
+    Idea: asValue(Idea),
+    Comment: asValue(Comment)
+});
 
 module.exports = container;
