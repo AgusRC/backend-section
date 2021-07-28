83df58624c47d469caacafb7712c6cc208541951
AgusRC <agustinmetal_@hotmail.com>
2021-07-28 12:43:36 -0500

    [ADD] test added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+        _id: "62306f0250974232fa535d43",
+        name: "ueue",
+        username: "jdjdhf",
+        password: "jsjcijheoiejdj"
+    },
+    users: [
+        {
+            _id: "62657f0254744232fa775d43",
+            name: "lsls",
+            username: "jlslsls",
+            password: "jfesdejdj"
+        },
+        {
+            _id: "62346f0550954232fa535d43",
+            name: "sdfguio",
+            username: "tuiuyi",
+            password: "j7887heoiejdj"
+        }
+    ]
+}
